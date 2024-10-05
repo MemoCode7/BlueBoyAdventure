c6b8af431766205b19e5b18b9472ddd583a155d5
SunnyThakur-7 <sunnyking8691@gmail.com>
2024-10-05 18:09:53 +0530

    added AssetSetter added object package , added objects

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,10 +1,8 @@
 package main;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 import java.awt.*;
@@ -32,7 +30,10 @@ public class GamePanel extends JPanel implements Runnable{
 
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+    
     
     int playerX = 100;
     int playerY = 100;
@@ -45,6 +46,10 @@ public class GamePanel extends JPanel implements Runnable{
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+    }
+
+    public void setUpGame(){
+        aSetter.setObject();
     }
 
     public void startGameThread(){
@@ -90,7 +95,17 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
+        //TILE
         tileM.draw(g2);
+
+        //OBJECT
+        for(int i = 0;i< obj.length ; i++){
+            if(obj[i] != null){
+                obj[i].draw(g2, this);
+            }
+        }
+
+        //PLAYER
         player.draw(g2);
         g2.dispose();
     }
