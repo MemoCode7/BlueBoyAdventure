1f467514cb14416f3fcfbad5381cc5cd3cb77bb4
SunnyThakur-7 <sunnyking8691@gmail.com>
2024-10-09 02:38:21 +0530

    added events of pit damage,healing pool and teleportation video #19 completed

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -92,6 +92,10 @@ public class Player extends Entity {
             //check npc collison
             int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
             interactNPC(npcIndex);
+
+            // CHECK EVENT
+            gp.eHandler.checkEvent();
+            gp.keyH.enterPressed = false;
             //IF COLLISION IS FALSE , PLAYER CAN MOVE
             if(collisionOn == false){
                 switch (direction) {
@@ -136,7 +140,7 @@ public class Player extends Entity {
                 gp.npc[i].speak();
             }   
         }
-        gp.keyH.enterPressed = false;
+
     }
 
     public void draw(Graphics2D g2){
